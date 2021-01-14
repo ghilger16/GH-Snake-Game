c3fffc670b22b80c49b85f85e2293ad7885a5491
ghilger16 <ghilger16@yahoo.com>
2021-01-13 17:56:09 -0700

    added canvas

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+let canvas = document.getElementById("gameCanvas");
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "0a111c";
+ctx.fillRect(200, 100, canvas.width / 2, canvas.height / 1.5);

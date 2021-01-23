67bc53f6cbe3a2e270a67ea0c9b5b546e1bb4d08
ghilger16 <ghilger16@yahoo.com>
2021-01-22 17:57:02 -0700

    added game over modal and high score local storage

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let canvas = document.getElementById("gameCanvas");
 let ctx = canvas.getContext("2d");
 let scoreDisplay = document.getElementById("score");
+let highScoreDisplay = document.getElementById("highScore");
+let playAgainBtn = document.getElementById("playAgainBtn");
 
 let bw = 300;
 let bh = 150;
@@ -24,7 +26,11 @@ let snakeY = snake[0].y;
 let snakeDirection;
 
 let score = 0;
-scoreDisplay.textContent = `Score = ${score}`;
+let highScore = localStorage.getItem("highScore");
+
+playAgainBtn.addEventListener("click", () => {
+  document.location.reload();
+});
 
 document.addEventListener("keydown", keyDownHandler, false);
 
@@ -46,7 +52,11 @@ window.onload = function () {
     drawEverything();
     moveEverything();
     if (boundaryDetection()) {
-      console.log("hit");
+      gameOver();
+    }
+    scoreDisplay.textContent = `Score ${score}`;
+    if (highScore) {
+      highScoreDisplay.textContent = `High Score ${highScore}`;
     }
   }, 1000 / framesPerSecond);
 };
@@ -87,31 +97,33 @@ function drawEverything() {
 }
 
 function moveEverything() {
-  if (snakeDirection == "right") {
-    snakeX += gridUnit;
-    console.log(snakeX);
-  } else if (snakeDirection == "left") {
-    snakeX -= gridUnit;
-    console.log(snakeX);
-  } else if (snakeDirection == "up") {
-    snakeY -= gridUnit;
-  } else if (snakeDirection == "down") {
-    snakeY += gridUnit;
-  }
-  snake.pop();
-  let snakeHead = {
-    x: snakeX,
-    y: snakeY,
-  };
-  snake.unshift(snakeHead);
-
-  if (snakeX == food.x && snakeY == food.y) {
-    score++;
-    snake.unshift(snakeHead);
-    food = {
-      x: Math.floor((Math.random() * bw) / gridUnit) * gridUnit,
-      y: Math.floor((Math.random() * bh) / gridUnit) * gridUnit,
+  if (!boundaryDetection()) {
+    if (snakeDirection == "right") {
+      snakeX += gridUnit;
+      console.log(snakeX);
+    } else if (snakeDirection == "left") {
+      snakeX -= gridUnit;
+      console.log(snakeX);
+    } else if (snakeDirection == "up") {
+      snakeY -= gridUnit;
+    } else if (snakeDirection == "down") {
+      snakeY += gridUnit;
+    }
+    snake.pop();
+    let snakeHead = {
+      x: snakeX,
+      y: snakeY,
     };
+    snake.unshift(snakeHead);
+
+    if (snakeX == food.x && snakeY == food.y) {
+      score++;
+      snake.unshift(snakeHead);
+      food = {
+        x: Math.floor((Math.random() * bw) / gridUnit) * gridUnit,
+        y: Math.floor((Math.random() * bh) / gridUnit) * gridUnit,
+      };
+    }
   }
 }
 
@@ -127,5 +139,16 @@ function boundaryDetection() {
   for (let i = 4; i < snake.length; i++) {
     if (snakeDirection && snake[i].x === snakeX && snake[i].y === snakeY)
       return true;
+  }
+}
+
+function gameOver() {
+  document.querySelector(".gameOverModal").classList.remove("hidden");
+  if (highScore !== null) {
+    if (score > highScore) {
+      localStorage.setItem("highScore", score);
+    }
+  } else {
+    localStorage.setItem("highScore", score);
   }
 }
